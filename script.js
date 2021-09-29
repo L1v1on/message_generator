f6413ca6ddb4c7380ee09dc53a1602ec8710838a
Ezra Vance <3zravance@gmail.com>
2021-09-29 11:22:42 -0400

    Store messages in seperate arrays

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('test');
+const beginning = ["They ", "You ", "People "];
+const middle = ["are ", "should be", "are not "];
+const end = ["smart!", "dumb!", "fast!"];
+console.log(beginning[0] + middle[0] + end[0]);
